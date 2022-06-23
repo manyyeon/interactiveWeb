f77b3b0cc13162bc99737d24192ba83b5a0608b8
manyyeon <udayeon@naver.com>
2022-06-23 19:35:56 +0900

    action is not a function 에러

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 (() => {
+  const actions = {
+    birdFiles(key) {
+      if (key) {
+        document.querySelector(
+          '[data-index="2"] .bird'
+        ).style.transform = `translateX(${window.innerWidth}px)`;
+      } else {
+        document.querySelector(
+          '[data-index="2"] .bird'
+        ).style.transform = `translateX(-100%)`;
+      }
+    },
+    birdFiles2(key) {
+      if (key) {
+        document.querySelector(
+          '[data-index="5"] .bird'
+        ).style.transform = `translate(${window.innerWidth}px, ${
+          -window.innerHeight * 0.7
+        }px)`;
+      } else {
+        document.querySelector(
+          '[data-index="5"] .bird'
+        ).style.transform = `translateX(-100%)`;
+      }
+    },
+  };
+
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
   let currentItem = graphicElems[0]; // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
@@ -15,18 +42,25 @@
     graphicElems[i].dataset.index = i;
   }
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add("visible");
+    if (action) {
+      console.log(action);
+      actions[action](true);
+    }
   }
 
-  function inactivate() {
+  function inactivate(action) {
     currentItem.classList.remove("visible");
+    if (action) {
+      actions[action](false);
+    }
   }
 
   window.addEventListener("scroll", () => {
     let step;
     let boundingRect;
-    for (let i = ioIndex - 1; i <= ioIndex + 1; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
       if (!step) {
         continue;
@@ -36,11 +70,15 @@
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
-        inactivate();
+        inactivate(currentItem.dataset.action);
         currentItem = graphicElems[step.dataset.index];
-        activate();
+        activate(currentItem.dataset.action);
       }
     }
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => scrollTo(0, 0), 100);
   });
 
   activate();
